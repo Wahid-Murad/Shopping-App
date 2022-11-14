16ab58d282535de8bc96e87ca5c498353b0e178c
Wahid_Murad <wahidmurad2000@gmail.com>
2022-11-14 20:03:28 +0600

    first commit

---
+++ lib/controller/productcontroller.dart
@@ -0,0 +1,26 @@
+import 'package:ecommerce/model/product.dart';
+import 'package:ecommerce/services/remote_services.dart';
+import 'package:get/state_manager.dart';
+
+class ProductController extends GetxController {
+  var isLoading = true.obs;
+  var productList = <Product>[].obs;
+
+  @override
+  void onInit() {
+    fetchProducts();
+    super.onInit();
+  }
+
+  void fetchProducts() async {
+    try {
+      isLoading(true);
+      var products = await RemoteServices.fetchProducts();
+      if (products != null) {
+        productList.value = products;
+      }
+    } finally {
+      isLoading(false);
+    }
+  }
+}
